3ab213464c49c3a2cbd26b3344b6c406dc8efdc3
EndersonSS <eendersonserra@gmail.com>
2020-05-23 12:01:03 -0300

    atualizando projeto

---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,66 @@
+import 'package:app_despesas/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+  
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index){
+          final tr = transactions[index];
+          return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 22,
+                        vertical: 22,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.redAccent[200],
+                          width: 4,
+                          )
+                      ),
+                      child: Text(
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28,
+                          color: Colors.red,
+                        )
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(tr.title,
+                        style:TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),),
+                        Text(
+                          DateFormat('dMMMY').format(tr.date),
+                            style: TextStyle(
+                              fontSize:15,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+        },    
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:math';
+
+import 'package:app_despesas/components/transaction_form.dart';
+import 'package:app_despesas/components/transaction_list.dart';
 import 'package:app_despesas/models/transaction.dart';
 import 'package:flutter/material.dart';
+
 
 main() => runApp(ExpensesApp());
 
@@ -11,49 +16,72 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+ 
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
   
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
   final _transactions = [
     Transaction(
       id: 't1',
       title: 'Novo Tênis',
       value: 200.00,
-      date: DateTime.now()
+      date: DateTime.now()  
       ),
-    Transaction(
-      id: 't1',
-      title: 'Conta de Luz',
-      value: 100.00,
-      date: DateTime.now()
-      ),
-  ];  
+  ]; 
+
+  _addTransaction(String title, double value){
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
   
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+   _openTransactionFormModal(BuildContext context){
+   showModalBottomSheet(
+     context: context, 
+     builder: (_){
+      return TransactionForm(_addTransaction);
+     });
+ }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais')
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround, 
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            child: Card(
-              color: Colors.blueAccent,
-              child: Text('Grafico'),
-              elevation: 6,
-            )
-          ),
-          Column(
-            children:_transactions.map((tr){
-              return Card(
-                child: Text(tr.title),
-              );
-              }).toList()
-          )
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blueAccent,
+                child: Text('Grafico'),
+                elevation: 6,
+              )
+            ),
+            TransactionList(_transactions)
         ],
       ),
+   ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _openTransactionFormModal(context),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
